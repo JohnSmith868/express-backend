5a9def5729f68c283413d48f295e6d7e09e3dc3d
johnsmith868 <john@john.com>
2021-07-18 22:12:19 +0800

    Fix mysql

--- app.js
+++ app.js
@@ -5,8 +5,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const config = require('./src/config/config');
 
-const indexRouter = require('./src/routes/v1/index');
-const usersRouter = require('./src/routes/v1/users');
+const routes = require('./src/routes/v1/index');
 
 const app = express();
 
@@ -19,9 +18,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 
 //mysql connection
@@ -38,14 +34,20 @@ const pool = mysql.createPool({
 pool.getConnection(function (err, connection) {
   if (err) throw err;
   console.log('mysql database connected;');
-  
+
 });
 
 //mysql state
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   req.mysqlpool = pool;
   next();
 });
+
+//auth services
+app.use('/auth/v1', routes);
+
+
+
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
